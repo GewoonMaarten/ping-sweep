6200fbe6a0a9c946a90947e1cae13b93c1c7df66
Maarten <maarten@deklerk.dev>
2025-06-01 22:10:40 +0200

    feat: add error handling and add timeout

--- src/ping/icmp_socket.zig
+++ src/ping/icmp_socket.zig
@@ -1,24 +1,61 @@
 const std = @import("std");
 const icmp = @import("icmp_header.zig");
+const ip = @import("ip.zig");
 
 pub const IcmpSocket = struct {
     socket: i32 = 0,
 
-    pub fn init() !IcmpSocket {
-        const socket = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.RAW, std.posix.IPPROTO.ICMP);
+    pub fn init(receiveTimeout: i32) !IcmpSocket {
+        const socket = try std.posix.socket(
+            std.posix.AF.INET,
+            std.posix.SOCK.RAW,
+            std.posix.IPPROTO.ICMP,
+        );
         errdefer std.posix.close(socket);
+
+        const tv: std.posix.timeval = .{
+            .sec = receiveTimeout,
+            .usec = 0,
+        };
+        try std.posix.setsockopt(
+            socket,
+            std.posix.SOL.SOCKET,
+            std.posix.SO.RCVTIMEO,
+            std.mem.asBytes(&tv),
+        );
+
         return IcmpSocket{ .socket = socket };
     }
 
-    pub fn sendTo(self: *const IcmpSocket, header: icmp.IcmpHeader, address: std.net.Address) !void {
-        const result = try std.posix.sendto(self.socket, @as(*const [8]u8, @ptrCast(&header)), 0, &address.any, address.getOsSockLen());
-        std.debug.print("sendTo sent {d} bytes\n", .{result});
+    pub fn sendTo(self: *const IcmpSocket, header: icmp.IcmpHeader, address: ip.Ip) !void {
+        while (true) {
+            _ = std.posix.sendto(
+                self.socket,
+                @as(*const [8]u8, @ptrCast(&header)),
+                0,
+                &address.address.any,
+                address.address.getOsSockLen(),
+            ) catch |err| {
+                if (err == error.SystemResources) {
+                    // std.debug.print("System overloaded, waiting...\n", .{});
+                    std.Thread.sleep(100_000);
+                    continue;
+                }
+                return err;
+            };
+            break;
+        }
     }
 
-    pub fn recv(self: *const IcmpSocket) 28[u8] {
-        var buffer: 28[u8] = undefined;
-        const result = try std.posix.recv(self.socket, &buffer, 0);
-        std.debug.print("recv received {d} bytes\n", .{result});
+    pub fn recv(self: *const IcmpSocket) ?[28]u8 {
+        var buffer: [28]u8 = undefined;
+        _ = std.posix.recv(self.socket, &buffer, 0) catch |err| {
+            if (err == std.posix.RecvFromError.WouldBlock) {
+                std.debug.print("recv timeout\n", .{});
+            }
+            return null;
+        };
+        // std.debug.print("recv received {d} bytes\n", .{result});
         return buffer;
     }
 };
